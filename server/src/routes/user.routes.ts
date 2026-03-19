bfce73ac68eff525081e6aab4ed1cc7cf17d25dc
CODECZERO <codeczero@users.noreply.github.com>
2026-03-19 10:33:06 +0530

    feat(routing): define API endpoints for stats.routes (and 4 related files)

---
+++ server/src/routes/user.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { signup, login, logout, getMe } from '../controler/userNgo.controler.js';
+import { verifyJWT } from '../midelware/verify.midelware.js';
+
+const router = Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/logout', logout);
+router.get('/me', verifyJWT, getMe);
+
+export default router;
